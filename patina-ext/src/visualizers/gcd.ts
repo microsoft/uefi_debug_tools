1806df0b3fe787982bb046a6811cccf5685e814f
Joey Vagedes <joeyvagedes@microsoft.com>
2026-02-24 11:13:25 -0800

    update

--- patina-ext/src/visualizers/gcd.ts
+++ patina-ext/src/visualizers/gcd.ts
@@ -50,19 +50,47 @@ namespace Visualizers.Gcd {
         }
 
         // Exposes the `attributes` field of the underlying enum as a property of the `MemoryBlock` enum.
-        get attributes(): number {
-            return this.__0.attributes;
+        get attributes(): string {
+            return this.attr_to_str(this.__0.attributes);
         }
 
         // Exposes the `capabilities` field of the underlying enum as a property of the `MemoryBlock` enum.
-        get capabilities(): number {
-            return this.__0.capabilities;
+        get capabilities(): string {
+            return this.attr_to_str(this.__0.capabilities);
         }
 
         // Convert the tag (0x0 or 0x1) to the corresponding string ("Unallocated" or "Allocated")
         tag_str(): string {
             const tagStr = `${this.tag}`;
             return tagStr === "0x0" ? "Unallocated" : "Allocated";
+        }
+
+        // Helper method to perform a bitwise AND operation on a int64
+        private hasFlag(attr: any, maskStr: any) {
+            const mask = host.parseInt64(maskStr, 16);
+            return attr.bitwiseAnd(mask).compareTo(0) != 0;
+        }
+
+        // Helper method to convert the attributes and capabilities bitfields into a human-readable string.
+        private attr_to_str(attr: any) {
+            const attrs = [];
+
+            if (this.hasFlag(attr, "0x1")) attrs.unshift("UC");
+            if (this.hasFlag(attr, "0x2")) attrs.unshift("WC");
+            if (this.hasFlag(attr, "0x4")) attrs.unshift("WT");
+            if (this.hasFlag(attr, "0x8")) attrs.unshift("WB");
+            if (this.hasFlag(attr, "0x10")) attrs.unshift("UCE");
+            if (this.hasFlag(attr, "0x1000")) attrs.unshift("WP");
+            if (this.hasFlag(attr, "0x2000")) attrs.unshift("RP");
+            if (this.hasFlag(attr, "0x4000")) attrs.unshift("XP");
+            if (this.hasFlag(attr, "0x8000")) attrs.unshift("NV");
+            if (this.hasFlag(attr, "0x10000")) attrs.unshift("MR");
+            if (this.hasFlag(attr, "0x20000")) attrs.unshift("RO");
+            if (this.hasFlag(attr, "0x40000")) attrs.unshift("SP");
+            if (this.hasFlag(attr, "0x80000")) attrs.unshift("CC");
+            if (this.hasFlag(attr, "0x8000000000000000")) attrs.unshift("RT");
+
+            return attrs.join("|") || "None";
         }
     }
 
